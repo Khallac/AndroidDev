2c4eacfd01b8794cbaa29923c9dd7879ff9369ab
katie <katie@DESKTOP-46U730A.home>
2020-08-03 16:00:46 -0400

    Android stuff

--- AndroidAppgameTic/src/main/java/UnaryOperator.java
+++ AndroidAppgameTic/src/main/java/UnaryOperator.java
@@ -12,9 +12,11 @@ public class UnaryOperator {
     public static void main(String []args){
         double number = 5.2, resultNumber;
         boolean flag = false;
-        System.out.println("+number = "+ +number);
-        System.out.println("+number = " + -number);
-        
+        System.out.println("number = "+ +number);
+        System.out.println("number = " + -number);
+        System.out.println("number = " + ++number);
+        System.out.println("number = " + --number);
+        System.out.println(flag);
                 
                 
     }
