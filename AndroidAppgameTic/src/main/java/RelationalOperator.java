633eabaef940b6f38d2d078182587307d1f0aa85
katie <katie@DESKTOP-46U730A.home>
2020-08-04 11:07:38 -0400

    RelationalOperator

---
+++ AndroidAppgameTic/src/main/java/RelationalOperator.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+public class RelationalOperator {
+    public static void main(String []args){
+        int num1 = 8;
+        int num2 = 21;
+        if (num2 > num1){
+            System.out.println("Number 2 is greater then Number 1");   
+    }else{
+            System.out.println("Number 1 is greater then Number 2");
+        }
+        
+                
+    }
+}
