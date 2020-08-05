f77666cac90fb430bf0845802c1474aab806c1cb
katie <katie@DESKTOP-46U730A.home>
2020-08-05 09:45:03 -0400

    Input

--- AndroidAppgameTic/src/main/java/Input.java
+++ AndroidAppgameTic/src/main/java/Input.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,11 +14,10 @@
 import java.util.Scanner;
 public class Input {
     public static void main (String []args){
-        Scanner input = new Scanner (System.in);
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your favorate number?");
         int number = input.nextInt();
-        System.out.println(number);
-        input.close();
-        
-
-    }
+        System.out.println("you entered:" + number);
+        input.close();   
+}
 }
