529dc61742beac9910343f3f69d38d8c9f4a2e07
katie <katie@DESKTOP-46U730A.home>
2020-08-05 10:59:22 -0400

    Ladder

---
+++ AndroidAppgameTic/src/main/java/Ladder.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+public class Ladder {
+    public static void main(String []args){
+        int number1 = 0;
+        if(number1 > 0){
+        System.out.println("This number is positive. ");
+        }
+        if(number1 < 0){
+            System.out.println("This number is negative. ");
+        }else{
+            System.out.println("This number is equal to zero. ");
+        }
+    }
+}
