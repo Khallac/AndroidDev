038a9d731002cac60acef9c93d0e447dfbba7664
katie <katie@DESKTOP-46U730A.home>
2020-08-05 10:47:45 -0400

    IfElse

---
+++ AndroidAppgameTic/src/main/java/IfElse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+public class IfElse {
+    public static void main(String []args){
+        int number1 = -10;
+        if (number1 > 0){
+            System.out.println("This number is positive. ");
+        }
+        if (number1 < 0){
+            System.out.println("This number is negative. ");
+        }
+        
+    }
+}
