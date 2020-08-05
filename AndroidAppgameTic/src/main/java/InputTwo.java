94404cdfe9e478c3ee9b9d745f0d6e86006b3dc3
katie <katie@DESKTOP-46U730A.home>
2020-08-05 13:04:25 -0400

    Input2

--- AndroidAppgameTic/src/main/java/InputTwo.java
+++ AndroidAppgameTic/src/main/java/InputTwo.java
@@ -11,13 +11,13 @@
 import java.util.Scanner;
 public class InputTwo {
     public static void main (String []args){
-        Scanner input = new Scanner(System.in);
-        double floatnum1 = 3.5;
-        System.out.println(floatnum1);
-        double doublenum1 = 15.2;
-        System.out.println(doublenum1);
-        String txt = "What is your favorite number? ";
-        System.out.println(txt);
+         Scanner input = new Scanner(System.in);
+       System.out.println("enter float");
+       float myFloat = input.nextFloat();
+       System.out.println("float entered =" + myFloat );
+       input.close();
+             
                       
 }
 }
+ 
