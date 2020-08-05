6385144dc921fc583fc88c7b5a124d9d4166e072
katie <katie@DESKTOP-46U730A.home>
2020-08-05 14:24:44 -0400

    Testing

--- AndroidAppgameTic/src/main/java/Testing.java
+++ AndroidAppgameTic/src/main/java/Testing.java
@@ -12,7 +12,10 @@ public class Testing {
     public static void main (String []args){
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         cars [0] = "opel";
-        System.out.println(cars[0]);
+        cars [1] = "BMW";
+        cars [2] = "Ford";
+        cars [3] = "Mazda";
+        System.out.println(cars[0] + cars[1] + cars[2]+ cars[3]);
         
         }
     }
