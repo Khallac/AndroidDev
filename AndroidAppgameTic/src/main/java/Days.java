fc429c8e16e7200e23ea8848c5e0990805aaa726
katie <katie@DESKTOP-46U730A.home>
2020-08-05 11:17:21 -0400

    Days

---
+++ AndroidAppgameTic/src/main/java/Days.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+public class Days {
+    public static void main(String []args){
+        int week = 5;
+        String day;
+        switch (week){
+            case 1:
+            day = "sunday";
+            break;
+            case 2:
+            day = ("monday");
+            break;
+            case 3:
+            day = ("tuesday");
+            break;
+            case 4:
+            day = ("wednesday");
+            case 5:
+            day = ("thursday");
+            System.out.println("The 5th day of the week is " + day );
+            
+             
+                    
+            
+        }
+    }
+}
