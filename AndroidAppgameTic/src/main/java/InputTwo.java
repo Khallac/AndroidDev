ed089d66a69137d683a9c04d03508e709ef7addf
katie <katie@DESKTOP-46U730A.home>
2020-08-05 10:38:12 -0400

    Input2

---
+++ AndroidAppgameTic/src/main/java/InputTwo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+import java.util.Scanner;
+public class InputTwo {
+    public static void main (String []args){
+        Scanner input = new Scanner(System.in);
+        double floatnum1 = 3.5;
+        System.out.println(floatnum1);
+        double doublenum1 = 15.2;
+        System.out.println(doublenum1);
+        String txt = "What is your favorite number? ";
+        System.out.println(txt);
+                      
+}
+}
