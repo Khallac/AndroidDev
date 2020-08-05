d5ed61bd1f6a1899e8a889239fb3933b35c077e6
katie <katie@DESKTOP-46U730A.home>
2020-08-05 14:17:04 -0400

    MultiArray

---
+++ AndroidAppgameTic/src/main/java/MultiArray.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author katie
+ */
+public class MultiArray {
+    public static void main(String []args) {
+        int [][] a = {
+            {1,2,3},
+            {4,5,6,7},  };
+            System.out.println("Length of row 1: " + a [0].length);
+            System.out.println("Length of row 2: " + a [1].length );
+            
+        
+        
+                
+        }
+        
+    } 
+
